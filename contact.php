09170a9a30abc9a8dbdaca8c7d1baa91c3ead220
Paul Madden <p.madden90@gmail.com>
2018-12-18 16:24:18 +0000

    Added content

--- contact.php
+++ contact.php
@@ -1,40 +1,29 @@
 <?php
-/*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
+
 
 require 'PHPMailer-master/PHPMailerAutoload.php';
 
-/*
-*  CONFIGURE EVERYTHING HERE
-*/
 
-// an email address that will be in the From field of the email.
 $fromEmail = 'demo@domain.com';
 $fromName = 'Demo contact form';
 
-// an email address that will receive the email with the output of the form
+
 $sendToEmail = 'bouncebackphysiotherapy@mail.com';
 $sendToName = 'Contact Form';
 
-// subject of the email
+
 $subject = 'New message from contact form';
 
-// form field names and their translations.
-// array variable name => Text to appear in the email
+
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 
-// message that will be displayed when everything is OK :)
+
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
-// If something goes wrong, we will display this message.
+
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-/*
- *  LET'S DO THE SENDING
- */
 
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 try
@@ -59,7 +48,7 @@ try
     );
     
     // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    mail($sendToEmail, $subject, $emailText, implode("\n", $headers));
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
